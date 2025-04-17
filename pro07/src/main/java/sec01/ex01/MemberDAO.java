cda685960faea8a9bd24087dd0210f3486f47ac7
JA308 <JA308@DESKTOP-MK6UEQU>
2025-04-17 17:14:39 +0900

    25-04-17

---
+++ pro07/src/main/java/sec01/ex01/MemberDAO.java
@@ -0,0 +1,60 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+	private static final String driver = "oracle.jdbc.driver.OracleDriver";
+	private static final String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String user = "system";		// 계정 정보
+	private static final String pwd = "12345";
+	private Connection con;
+	private Statement stmt;
+	public List<MemberVO> listMembers() {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			connDB();		// 위 네가지 정보로 데이터베이스 연결
+			String query = "select * from t_member ";
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);		// sql문으로 회원정보 조회
+			while (rs.next()) {
+				String id = rs.getString("id");			// 조회한 레코드의 각 컬럽값 받아옴
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO();
+				vo.setId(id);					// 받아온 컬럼값 MemberVO객체에 설정
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);			// ArrayList에 설정한 MemberVO객체 저장
+			}
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;					// 조회한 레코드의 개수만큼 MemberVO 객제를 저장한 ArrayList 반환
+	}
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle 드라이버 로딩 성공");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+			stmt = con.createStatement();
+			System.out.println("Statement 생성 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
