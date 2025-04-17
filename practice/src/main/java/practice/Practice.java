7c95c701d7784c9dffd4f67cfd4fa6b2b1b74a6e
JA308 <JA308@DESKTOP-MK6UEQU>
2025-04-17 17:20:12 +0900

    25-04-17

---
+++ practice/src/main/java/practice/Practice.java
@@ -0,0 +1,48 @@
+package practice;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/practice")
+public class Practice extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
+
+// 서브릿은 메인 메서드 안씀
+// import 자동추가 ctrl + shift + o
+// servlet 파일로 생성
+// web.xml 에 매핑할 필묘없이 생성
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
