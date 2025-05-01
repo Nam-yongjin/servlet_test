61f977238cc27c880da4893e40b103dd2da6549a
JA308 <JA308@DESKTOP-MK6UEQU>
2025-05-01 17:18:54 +0900

    25-05-01

--- pro17/src/main/java/sec01/ex01/MemberController.java
+++ pro17/src/main/java/sec01/ex01/MemberController.java
@@ -1,33 +1,43 @@
 package sec01.ex01;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MemberController
- */
-@WebServlet("/MemberController")
+
+@WebServlet("/mem.do")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	MemberDAO memberDAO;
+	
+	public void init() throws ServletException {
+		memberDAO = new MemberDAO();	// 실행할때 MemberDAO 미리 실행
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");	// 한글처리
+		
+		List<MemberVO> membersList = memberDAO.listMembers();	// DAO로 받아온 데이터 list형식으로 받음
+		request.setAttribute("membersList", membersList);	// 요청한 데이터 set 저장
+		// listMembers에 dispatch로 전달
+		RequestDispatcher dispatch = request.getRequestDispatcher("/test01/listMembers.jsp");
+		dispatch.forward(request, response);	// dispatch로 포워딩
 	}
 
 }
--- pro17/src/main/webapp/test01/listMembers.jsp
+++ pro17/src/main/webapp/test01/listMembers.jsp
@@ -1,12 +1,66 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+	import=" java.util.*,sec01.ex01.*"
+	pageEncoding="UTF-8"
+	isELIgnored="false"%> 	<%-- EL문 사용 --%>
+<%-- JSTL의 국제화 및 포맷팅(formatting) 태그를 사용 --%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %> 
+<%--  JSTL의 Core(핵심) 태그들을 사용하겠다는 선언 (ex  if, forEach, when, out) --%>   
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%
+request.setCharacterEncoding("UTF-8");	// 한글인코딩
+%>    
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+   <meta  charset="UTF-8">
+   <title>회원 정보 출력창</title>
+<style>
+     .cls1 {
+       font-size:40px;
+       text-align:center;
+     }
+    
+     .cls2 {
+       font-size:20px;
+       text-align:center;
+     }
+  </style>
+  
 </head>
 <body>
-
+ <p class="cls1">회원정보</p>
+   <table align="center" border="1" >
+      <tr align="center" bgcolor="lightgreen">
+         <td width="7%" ><b>아이디</b></td>
+         <td width="7%" ><b>비밀번호</b></td>
+         <td width="7%" ><b>이름</b></td>
+         <td width="7%"><b>이메일</b></td>
+         <td width="7%" ><b>가입일</b></td>
+   </tr>
+<%-- c:choose == switch --%>
+<c:choose>	
+	<%-- c:when == if --%>	
+    <c:when test="${ empty membersList}" >	<%-- 멤버list가 비어있다면 --%>
+      <tr>
+        <td colspan=5 align="center">
+          <b>등록된 회원이 없습니다.</b>
+       </td>  
+      </tr>
+   </c:when>  
+   <c:when test="${!empty membersList }" >
+   	  <%-- c:forEach == for --%>
+      <c:forEach  var="mem" items="${membersList }" >	<%-- memberList값을 mem변수로 반복문돌림 --%>
+        <tr align="center">
+          <td>${mem.id }</td>
+          <td>${mem.pwd }</td>
+          <td>${mem.name}</td>     
+          <td>${mem.email }</td>     
+          <td>${mem.joinDate}</td>     
+       </tr>
+     </c:forEach>
+</c:when>
+</c:choose>
+   </table>  
+ <a href="#"><p class="cls2">회원 가입하기</p></a>	<%-- 구현 안된 부분 --%>
 </body>
 </html>
+   
